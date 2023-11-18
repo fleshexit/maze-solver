c699fed11fafcf0f761c53a17ced23cc38fe06d0
chrlie-w <chrlie.w@proton.me>
2023-11-18 00:48:03 +0000

    dijkstra skeleton

--- algorithms/dijkstra.py
+++ algorithms/dijkstra.py
@@ -1,3 +1,11 @@
+from pathfinding import Pathfinder
 
+class Dijkstra:
 
-# Dijkstras pathfinding algorithm
+    def __init__(self, grid, start, end):
+        super().__init__(grid, start, end)
+        self.queue = []
+        self.closed_set = []
+        self.path = []
+        self.searching = True
+        self.search_started = False
