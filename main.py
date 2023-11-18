9cd90747fc2c99927055ae49772745e8d6a76590
chrlie-w <chrlie.w@proton.me>
2023-11-18 00:52:17 +0000

    Imports

--- main.py
+++ main.py
@@ -1,21 +1,14 @@
 import pygame
-import heapq
-
 import grid.cell as cell
 from grid.grid import Grid
 from mazes.maze import Mazes
 from config import Colors, Settings
+from algorithms.pathfinding import Pathfinder
 
 pygame.init()
 screen = pygame.display.set_mode((Settings.window_width, Settings.window_width))
 pygame.display.set_caption("Maze Solver")
-#icon = pygame.image.load('maze.png')
             
-
-queue = []
-closed_set = []
-path = []
-
 
 def main():
     gameboard = Grid(Settings.rows, Settings.window_width)
@@ -25,8 +18,6 @@ def main():
 
     
     terrain_generated = False
-    search_started = False
-    searching = True
     times_clicked = 0
 
     while True:
@@ -59,28 +50,8 @@ def main():
                         gameboard.place_mines()
                 if event.key == pygame.constants.K_r:
                     gameboard.reset()
-                if event.key == pygame.constants.K_f and end_selected:
-                    search_started = True
-        
-        if search_started:
-            if len(queue) > 0 and searching:
-                current_cell = queue.pop(0)
-                current_cell.visited = True
-                if current_cell == end_cell:
-                    searching = False
-                    while current_cell.prior != gameboard.start_cell:
-                        path.append(current_cell.prior)
-                        current_cell = current_cell.prior
-                else:
-                    for neighbour in current_cell.neighbours:
-                        if not neighbour.queued and not neighbour.blocked:
-                            neighbour.queued = True
-                            neighbour.prior = current_cell
-                            queue.append(neighbour)
-            else:
-                if searching:
-                    print("No path found")
-                    searching = False
+                #if event.key == pygame.constants.K_f and end_selected:
+                #    logic...
                     
         screen.fill(Colors.black)
         for i in range(Settings.cols):
@@ -88,8 +59,8 @@ def main():
                 cell = gameboard.grid[i][j]
                 cell.draw(screen, (Colors.white))    
 
-                if cell in path:
-                    cell.draw(screen, (Colors.green))
+                #if cell in path:
+                #    cell.draw(screen, (Colors.green))
                 if cell.queued:
                     cell.draw(screen, (255, 200, 0))
                     cell.draw(screen, (255, 175, 0), 1)
